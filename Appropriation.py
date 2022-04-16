340453317a1f384fd65b81d309fa6239464d0282
Jonathan <jamestfishes@gmail.com>
2022-04-16 09:02:39 -0400

    added the start of the project

---
+++ Appropriation.py
@@ -0,0 +1,75 @@
+import requests
+import urllib
+import base64
+import webbrowser
+from PIL import Image
+import numpy as np
+
+def face(arr,old):
+    face=arr[8:16,8:16]
+    f=Image.fromarray(face)
+    f.save("face.png")
+
+def front(arr,old):
+    front=np.copy(arr[0:32,0:16])
+    #white background
+    front[:,:,:]=0
+    #face
+    front[0:8,4:12] = arr[8:16,8:16]
+    #body
+    front[8:20,4:12] = arr[20:32,20:28]
+    #left arm
+    front[8:20,0:4] = arr[52:64,36:40]
+    #right arm
+    front[8:20,12:16] = arr[20:32,44:48]
+    #left leg
+    front[20:32,4:8] = arr[20:32,4:8]
+    #right leg
+    front[20:32,8:12] = arr[52:64,20:24]
+    f=Image.fromarray(front)
+    f.save("front.png")
+
+def side(arr,old):
+    side=np.copy(arr[0:32,0:16])
+    #white background
+    side[:,:,:]=0
+    #face
+    side[0:8,4:12] = arr[8:16,0:8]
+    #left leg
+    side[20:32,6:10] = arr[20:32,0:4]
+    #left arm
+    side[8:20,6:10] = arr[52:64,32:36]
+    f=Image.fromarray(side)
+    f.save("side.png")
+
+Username = input("Who do you want to steal the skin of?: ")
+UUID = requests.request("GET","https://api.mojang.com/users/profiles/minecraft/"+Username)
+UUID=UUID.json()["id"]
+print(UUID)
+skinlink = (requests.request("GET","https://sessionserver.mojang.com/session/minecraft/profile/"+UUID)).json()["properties"][0]["value"]
+skinlink=base64.b64decode(skinlink)
+skinlink=(skinlink.decode().split()[18]).split("\"")[1]
+print(skinlink)
+skin=urllib.request.urlretrieve(skinlink, "sample.png")
+
+im=Image.open("sample.png")
+im=np.array(im)
+print(type(np.shape(im)))
+if(np.shape(im)[0]<60):
+    old=True
+else:
+    old=False
+side(im,old)
+front(im,old)
+face(im,old)
+webbrowser.open("index.html")
+
+
+
+'''
+skinlink = (requests.request("GET","https://sessionserver.mojang.com/session/minecraft/profile/"+UUID)).json()["properties"][0]["value"]
+skinlink=base64.b64decode(skinlink)
+skinlink=(skinlink.decode().split()[18]).split("\"")[1]
+print(skinlink)
+skin=urllib.request.urlretrieve(skinlink, "sample.png")
+'''
